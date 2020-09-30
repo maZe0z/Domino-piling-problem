9221558379b3ae2df28d139f71aaa08a3116f9dd
Zakhar <38567289+maZe0z@users.noreply.github.com>
2020-09-30 23:12:36 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int N, M, number;
+    cin >> M >> N;
+    number = floor(N*M*0.5);
+    cout << number;
+    return 0;
+
+}
